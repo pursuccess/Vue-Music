021282f491f1f78e0d0ea563315361eb8d31a32d
pursuccess <976482580@qq.com>
2018-09-13 09:48:19 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import recommend from 'components/recommend/recommend'
+// import singer from 'components/singer/singer'
+// import rank from 'components/rank/rank'
+// import search from 'components/search/search'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      // 地址为空默认跳转首页
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      // 推荐
+      path: '/recommend',
+      name: 'recommend',
+      component: recommend
+    },
+    // {
+    //   // 页
+    //   path: '/singer',
+    //   name: 'singer',
+    //   component: singer
+    // },
+    // {
+    //   // 页
+    //   path: '/rank',
+    //   name: 'rank',
+    //   component: rank
+    // },
+    // {
+    //   // 搜索页
+    //   path: '/search',
+    //   name: 'search',
+    //   component: search
+    // }
+  ]
+})
