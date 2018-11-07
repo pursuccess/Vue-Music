51f7f608124d2fa984ee3d926eba54b6d7b88c05
pursuccess <976482580@qq.com>
2018-11-07 20:04:15 +0800

    歌曲播放列表删除

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -77,11 +77,12 @@
             <i @click.stop="togglePlaying" class="icon-mini" :class="miniIcon"></i>
           </progress-circle>
         </div>
-        <div class="control" @click.stop="">
+        <div class="control" @click.stop="showPlaylist">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
   </div>
 </template>
@@ -97,7 +98,7 @@
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
   //import {playerMixin} from 'common/js/mixin'
-  //import Playlist from 'components/playlist/playlist'
+  import Playlist from 'components/playlist/playlist'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
@@ -141,7 +142,7 @@
       },
     },
     watch: {
-      currentSong: {
+      /*currentSong: {
         handler(newVal, oldVal) {
           if (this.currentLyric) {
             this.currentLyric.stop()
@@ -152,6 +153,25 @@
           })
         },
         deep: true
+      },*/
+      currentSong(newSong, oldSong) {
+        if (!newSong.id) {
+          return
+        }
+        if (newSong.id === oldSong.id) {
+          return
+        }
+        if (this.currentLyric) {
+          this.currentLyric.stop()
+          this.currentTime = 0
+          this.playingLyric = ''
+          this.currentLineNum = 0
+        }
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          this.$refs.audio.play()
+          this.getLyric()
+        }, 1000)
       },
       playing(newPlaying) {
         const audio = this.$refs.audio
@@ -355,6 +375,9 @@
         }
         this.playingLyric = txt
       },
+      showPlaylist() {
+        this.$refs.playlist.show()
+      },
       middleTouchStart(e) {
         this.touch.initiated = true
         const touch = e.touches[0]
@@ -420,7 +443,8 @@
     components: {
       ProgressBar,
       ProgressCircle,
-      Scroll
+      Scroll,
+      Playlist
     }
   }
 </script>
